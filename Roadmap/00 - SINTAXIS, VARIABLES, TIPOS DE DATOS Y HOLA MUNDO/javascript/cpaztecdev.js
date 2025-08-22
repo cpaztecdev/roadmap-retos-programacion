416fec0ff30d414c7f4a9fa23a7f27aa8d840cb1
cpaztecdev <167780557+cpaztecdev@users.noreply.github.com>
2025-08-22 09:42:27 -0500

    update 22-08
    update

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/javascript/cpaztecdev.js
@@ -0,0 +1,28 @@
+/* documentacion oficial: https://developer.mozilla.org/es/docs/Web/JavaScript */
+
+// comentario de una linea
+
+/**
+ * comentario de varias lineas
+ */
+
+// Variables
+// let declara una variable que puede cambiar su valor
+let saludo = "Hola";
+
+// var declara una variable que puede cambiar su valor, no se recomienda usarla
+var saludo1 = "Hola Mundo";
+
+// Constantes
+const saludo2 = "Hola Mundo de javascript";
+
+// Tipos de datos primitivos
+let numero = 42; // Number
+let texto = "Hola"; // String
+let booleano = true; // Boolean
+let nulo = null; // Null
+let indefinido = undefined; // Undefined
+let decimal = 3.14; // Number
+
+// Mostrar por consola
+console.log("Hola, javascript!");
